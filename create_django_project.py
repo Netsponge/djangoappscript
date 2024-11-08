820832a07d614838a217c1a5e3cb86d445326425
jean-baptiste <139547916+Netsponge@users.noreply.github.com>
2024-11-08 10:21:47 +0100

    Update create_django_project.py

--- create_django_project.py
+++ create_django_project.py
@@ -2,13 +2,12 @@ import os
 import subprocess
 import sys
 
-# Nom du projet et chemins des dossiers
+# root project and name project
 PROJECT_NAME = "my_project"
 BASE_DIR = os.getcwd() + f"/{PROJECT_NAME}"
-VENV_DIR = os.path.join(BASE_DIR, '.venv')  # Chemin de l'environnement virtuel
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")  # Dossier des templates
+VENV_DIR = os.path.join(BASE_DIR, '.venv')  # root of virtual environmpent
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")  # templates folder
 
-# Contenu du fichier .gitignore
 GITIGNORE_CONTENT = """
 my_project
 .venv
@@ -17,38 +16,38 @@ __pycache__
 """
 
 def create_directory(path):
-    """Crée un dossier s'il n'existe pas."""
+    """create directory"""
     os.makedirs(path, exist_ok=True)
     print(f"Dossier créé: {path}")
 
 def create_virtual_environment():
-    """Crée un environnement virtuel dans le dossier du projet."""
+    """create virtual environment"""
     subprocess.run([sys.executable, "-m", "venv", VENV_DIR])
     print(f"Environnement virtuel créé dans {VENV_DIR}")
 
 def activate_virtual_environment():
-    """Active l'environnement virtuel du projet."""
+    """Activate virtual environment"""
     activate_script = os.path.join(VENV_DIR, "bin", "activate")
     # TODO: check under Windowss
     if sys.platform.startswith("win"):
         subprocess.run(["cmd", "/c", activate_script], check=True)
     else:
-        subprocess.run([".", activate_script, "&&", "echo", "Environnement virtuel activé"], shell=True, check=True)
+        subprocess.run([".", activate_script, "&&", "echo", "virtual environment activate!"], shell=True, check=True)
 
 def install_django():
-    """Installe Django dans l'environnement virtuel."""
+    """install django in virtual environment"""
     pip_path = os.path.join(VENV_DIR, 'bin', 'pip')
     subprocess.check_call([pip_path, 'install', 'django'])
     print("Django installé dans l'environnement virtuel.")
 
 def start_django_project():
-    """Initialise le projet Django avec `django-admin startproject`."""
+    """Initialise django project with`django-admin startproject`."""
     django_admin_path = os.path.join(VENV_DIR, 'bin', 'django-admin')
     subprocess.check_call([django_admin_path, 'startproject', "core", BASE_DIR])
     print(f"Projet Django '{PROJECT_NAME}' initialisé avec `manage.py`.")
 
 def create_gitignore():
-    """Crée un fichier .gitignore avec les règles spécifiées."""
+    """create gitignore file"""
     gitignore_path = os.path.join(BASE_DIR, ".gitignore")
     with open(gitignore_path, "w") as f:
         f.write(GITIGNORE_CONTENT)
@@ -56,7 +55,7 @@ def create_gitignore():
 
 
 def setup_project():
-    """Crée la structure de base du projet."""
+    """setup project"""
     print(f"Configuration du projet '{PROJECT_NAME}'...")
     create_directory(PROJECT_NAME)
     create_virtual_environment()
