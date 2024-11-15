0c4b65a4885452f76e29a08283b2b8c28ac4d3fd
jean baptiste marrec <jeanbaptiste.marrec@gmail.com>
2024-11-15 12:51:42 +0100

    add tal and npm

--- my_project/core/settings.py
+++ my_project/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
+    
 ]
  
