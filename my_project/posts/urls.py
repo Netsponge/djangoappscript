2e50cce11cbcdca738a0b9e8be58be38e58e3c9b
jean baptiste marrec <jeanbaptiste.marrec@gmail.com>
2024-11-15 09:15:25 +0100

    remove my_project

--- my_project/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.posts_list, name="posts"),
-    path('page/<slug:slug>/', views.post_page, name='page')
-
-] 
